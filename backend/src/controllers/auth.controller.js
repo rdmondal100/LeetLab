a6305be7f08aadd143d933649b107aab65fe6a1c
Riday Mondal <123723569+rdmondal100@users.noreply.github.com>
2025-05-18 20:24:40 +0600

    Riday (#4)
    * add: asyncHandler added
    
    * update: update the controler with asyncHandler

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -6,54 +6,50 @@ import { ApiError } from '../utils/api-error.js';
 import { ApiResponse } from '../utils/api-response.js';
 
 
-export const register = async (req, res,next) => {
+export const register = asyncHandler(async (req, res) => {
     const { email, password, name } = req.body;
-    try {
-        const existingUser = await db.user.findUnique({
-            where: {
-                email
-            }
-        })
-
-        if (existingUser) {
-            throw new ApiError(409, "User already exists")
+    const existingUser = await db.user.findUnique({
+        where: {
+            email
         }
+    })
 
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const newUser = await db.user.create({
-            data: {
-                email,
-                password: hashedPassword,
-                name,
-                role: UserRole.USER
-            }
-        })
-
-        const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, { expiresIn: "7d" })
-
-        res.cookie("jwt", token, {
-            httpOnly: true,
-            sameSite: "strict",
-            secure: process.env.NODE_env !== "development",
-            maxAge: 1000 * 60 * 60 * 24 * 7
-        })
-
-        const response = new ApiResponse(201, {
-            id: newUser.id,
-            email: newUser.email,
-            name: newUser.name,
-            role: newUser.role,
-            image: newUser.image
-        }, "User created successfully",
-        )
-
-        res.status(201).json(response)
-
-    } catch (error) {
-        console.log("Error creating user::", error)
-        next(error)
+    if (existingUser) {
+        throw new ApiError(409, "User already exists")
     }
-}
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = await db.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+            name,
+            role: UserRole.USER
+        }
+    })
+
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, { expiresIn: "7d" })
+
+    res.cookie("jwt", token, {
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV !== "development",
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    })
+
+    const response = new ApiResponse(201, {
+        id: newUser.id,
+        email: newUser.email,
+        name: newUser.name,
+        role: newUser.role,
+        image: newUser.image
+    }, "User created successfully",
+    )
+
+    res.status(response.statusCode).json(response)
+
+
+})
 export const login = async (req, res) => {
 
 }
