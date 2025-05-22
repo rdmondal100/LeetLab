fe4e2ca58a37c2cb006a061fd211fdd43598c1e7
Riday <rdmondal100@gmail.com>
2025-05-23 00:14:56 +0600

    add: all controllers structure add writen for playlist routes

---
+++ backend/src/controllers/playlist.controller.js
@@ -0,0 +1,9 @@
+import { asyncHandler } from "../utils/async-handler.js";
+
+
+export const getAllListDetails = asyncHandler(async(req,res )=>{})
+export const getPlayListDetails = asyncHandler(async(req,res )=>{})
+export const createPlayList = asyncHandler(async(req,res )=>{})
+export const addProblemToPlayList = asyncHandler(async(req,res )=>{})
+export const deletePlayList = asyncHandler(async(req,res )=>{})
+export const removeProblemFromPlayList = asyncHandler(async(req,res )=>{})
