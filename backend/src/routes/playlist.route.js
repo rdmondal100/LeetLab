ca577fe0716cef30794088b2e7f01130f7b5dc71
Riday <rdmondal100@gmail.com>
2025-05-23 01:05:58 +0600

    add: problemsIdValidator used in router

--- backend/src/routes/playlist.route.js
+++ backend/src/routes/playlist.route.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { checkAuthenticated } from '../middleware/authMiddleware.js'
 import { addProblemToPlayList, createPlayList, deletePlayList, getAllListDetails, getPlayListDetails, removeProblemFromPlayList } from '../controllers/playlist.controller.js'
-import { playListValidator } from '../validators/index.js'
+import { playListValidator, problemsIdValidator } from '../validators/index.js'
 
 const playlistRoutes = express.Router()
 
@@ -9,7 +9,7 @@ const playlistRoutes = express.Router()
 playlistRoutes.get("/", checkAuthenticated, getAllListDetails)
 playlistRoutes.get("/:playListId", checkAuthenticated, getPlayListDetails)
 playlistRoutes.post("/create-playList",playListValidator(), checkAuthenticated, createPlayList)
-playlistRoutes.post("/:playListId/add-problem", checkAuthenticated, addProblemToPlayList)
+playlistRoutes.post("/:playListId/add-problem",problemsIdValidator(), checkAuthenticated, addProblemToPlayList)
 playlistRoutes.delete("/:playListId", checkAuthenticated, deletePlayList)
 playlistRoutes.delete("/:playListId/remove-problem", checkAuthenticated, removeProblemFromPlayList)
 
