31d26ac3c672688b7c4be13e1f14d920f7c9c0cc
Riday <rdmondal100@gmail.com>
2025-05-22 20:46:43 +0600

    add: all routes are added

---
+++ backend/src/routes/submission.route.js
@@ -0,0 +1,15 @@
+
+import express from 'express'
+import { checkAuthenticated } from '../middleware/authMiddleware'
+import { getAllSubmission, getSubmissionCountForTheProblem, getSubmissionsForProblem } from '../controllers/submission.controller.js'
+
+const submissionRoutes = express.Router()
+
+submissionRoutes.get("/get-all-submissions", checkAuthenticated, getAllSubmission)
+
+submissionRoutes.get("/get-submission/:problemId", checkAuthenticated, getSubmissionsForProblem)
+
+submissionRoutes.get("/get-submissions-count/:problemId", checkAuthenticated, getSubmissionCountForTheProblem)
+
+
+export default submissionRoutes
