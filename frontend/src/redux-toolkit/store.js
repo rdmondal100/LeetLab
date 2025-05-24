c84280a4d1ed258f3a82dd8cdcbad2d355f306f9
Riday <rdmondal100@gmail.com>
2025-05-24 16:56:39 +0600

    fix: typo in middleware

--- frontend/src/redux-toolkit/store.js
+++ frontend/src/redux-toolkit/store.js
@@ -1,9 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './features/authSlice'
+import { authApi } from './services/authService'
+import { setupListeners } from '@reduxjs/toolkit/query'
 
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer
+    // Local slice reducer
+    auth: authReducer,
+
+
+    // API service reducer
+    [authApi.reducerPath]: authApi.reducer
   },
-})
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),})
+
+
+setupListeners(store.dispatch)
+
