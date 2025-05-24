8dcb231474b473ca8c3857d9dbc771732e9b5e5b
Riday <rdmondal100@gmail.com>
2025-05-24 15:09:01 +0600

    add: setup redux-tolkit

---
+++ frontend/src/redux-toolkit/features/authSlice.js
@@ -0,0 +1,21 @@
+
+
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  value: 0,
+}
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+   
+    
+  },
+})
+
+
+export const { } = authSlice.actions
+
+export default authSlice.reducer
