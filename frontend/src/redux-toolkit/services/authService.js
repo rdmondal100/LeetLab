7f07a9d40f02bd571b29519b9e593e55fac2626a
Riday <rdmondal100@gmail.com>
2025-05-24 16:57:16 +0600

    add: authServices completed

---
+++ frontend/src/redux-toolkit/services/authService.js
@@ -0,0 +1,31 @@
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { BASEURL } from '../../lib/constants'
+
+
+export const authApi = createApi({
+    reducerPath: "authApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: BASEURL,
+        credentials: 'include',
+        prepareHeaders: (headers) => {
+            headers.set('Content-Type', 'application/json')
+            return headers
+          }
+    }),
+    tagTypes: ['Auth'],
+
+    endpoints: (builder) => ({
+        registerNewUser: builder.mutation({
+            query: (newUser) => ({
+                url: '/register',
+                method: "POST",
+                body: newUser
+            })
+        })
+    }),
+    invalidatesTags: ['Auth']
+})
+
+
+export const { useRegisterNewUserMutation } = authApi
