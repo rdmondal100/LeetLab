e57b33f09bf62cc8fe3573ccbf80e7304d29a20f
Riday <rdmondal100@gmail.com>
2025-05-25 08:12:03 +0600

    update: change the backend port

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,2 +1,2 @@
 
-export const    BASEURL  =  import.meta.env.MODE === "development" ? "http://localhost:8081/api/v1": "/api/v1"
+export const    BASEURL  =  import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1": "/api/v1"
