3f0848997378c57c1f95b8429b6ef38aafd1d34b
Riday <rdmondal100@gmail.com>
2025-05-26 08:47:05 +0600

    add: implement RegisterUser functionality with toaster

--- frontend/src/pages/authPages/RegisterPage.jsx
+++ frontend/src/pages/authPages/RegisterPage.jsx
@@ -13,12 +13,18 @@ import {
 	FormLabel,
 	FormMessage,
 } from "@/components/ui/form";
+import { toast } from "sonner"
+
 import { Input } from "@/components/ui/input";
 import { registerFormSchema } from "../../schemas/registerFormSchema";
 import { Link } from "react-router-dom";
+import { useRegisterNewUserMutation } from "../../redux-toolkit/services/authService";
 
 const RegisterPage = () => {
 	const [showPassword, setShowPassword] = useState(false);
+
+    const [registerNewUser,{isLoading,isSuccess,isError,error}] = useRegisterNewUserMutation()
+
 
 	// 1. Define your form.
 	const form = useForm({
@@ -31,10 +37,34 @@ const RegisterPage = () => {
 	});
 
 	// 2. Define a submit handler.
-	const onSubmit = async (values) => {
-		// Do something with the form values.
-		// ✅ This will be type-safe and validated.
-		console.log(values);
+	const onSubmit = async (registerFormData) => {
+       
+		try {
+            const newUserData = {
+                name: registerFormData.name.trim(),
+                email: registerFormData.email.trim(),
+                password: registerFormData.password.trim(),
+            }
+            console.log(newUserData)
+            const response = await registerNewUser(newUserData).unwrap()
+      
+            console.log("User Registered:",response)
+            const successMessage = response.data.message || "User Registered Successfully"
+            toast.success(successMessage,{
+                className:"text-green-60 bg-green-100"
+            })
+            form.reset()
+        } catch (err) {
+            console.error("Registration failed:", err);
+
+            const errorMessage =
+              err?.data?.message || err?.error || "Something went wrong";
+        
+            toast.error(errorMessage,{
+                className:"text-destructive bg-destructive/50"
+            });
+        }
+        
 	};
 
 	return (
@@ -103,7 +133,10 @@ const RegisterPage = () => {
 						)}
 					/>
                  
-					<Button className=" w-full  cursor-pointer" type='submit'>Register</Button>
+                 <Button className="w-full" type='submit' disabled={isLoading}>
+  {isLoading ? (<div className="flex gap-1 justify-center items-center"><Loader2 className="animate-spin" size={18} />Registering..</div>) : ('Register')}
+</Button>
+
 				</form>
 			</Form>
 
