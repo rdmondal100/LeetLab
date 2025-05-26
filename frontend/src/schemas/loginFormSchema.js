97b81fc4db391a1fe02ba3c751ad0b73d7ea7318
Riday <rdmondal100@gmail.com>
2025-05-26 10:49:11 +0600

    update: remove the regex

--- frontend/src/schemas/loginFormSchema.js
+++ frontend/src/schemas/loginFormSchema.js
@@ -15,8 +15,5 @@ export const loginFormSchema = z.object({
     .string()
     .min(8, { message: "Password must be at least 8 characters long" })
     .max(20, { message: "Password must not exceed 20 characters" })
-    .regex(/[a-z]/, { message: "Password must include at least one lowercase letter" })
-    .regex(/[A-Z]/, { message: "Password must include at least one uppercase letter" })
-    .regex(/[0-9]/, { message: "Password must include at least one number" })
-    .regex(/[^A-Za-z0-9]/, { message: "Password must include at least one special character" }),
+     
 })
