d8863ad6c5d8d6ac30c908eacc3f6d96b21e9f64
Riday <rdmondal100@gmail.com>
2025-05-26 11:13:01 +0600

    fix: protected route logic handle all cases

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,36 +1,40 @@
-import React, { useEffect } from 'react'
-import { useGetAuthUserQuery } from '../redux-toolkit/services/authService';
-import { useDispatch } from 'react-redux';
-import { setAuthUser } from '../redux-toolkit/features/authSlice';
-import { Navigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useGetAuthUserQuery } from "../redux-toolkit/services/authService";
+import { useDispatch } from "react-redux";
+import { setAuthUser } from "../redux-toolkit/features/authSlice";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children, authentication = true }) => {
+	console.log(authentication);
+	const {
+		data: authUser,
+		isFetching,
+		isLoading,
+		error,
+	} = useGetAuthUserQuery();
+
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (authUser) {
+			dispatch(setAuthUser(authUser));
+		}
+	}, [authUser, dispatch]);
+
+	if (isLoading || isFetching) {
+		return <p>Loading...</p>; // Or show a spinner
+	}
+
+	if (authentication && !authUser && !isLoading && !isFetching) {
+		return <Navigate to='/login' replace />;
+	}
 
 
-const ProtectedRoute = ({ children, authentication = true}) => {
-    console.log(authentication)
-    const {
-        data: authUser,
-        isFetching,
-        isLoading,
-        error
-      } = useGetAuthUserQuery()
-
-      const dispatch = useDispatch()
-    
-      useEffect(() => {
-        if (authUser) {
-          dispatch(setAuthUser(authUser));
-        }
-      }, [authUser, dispatch]);
-
-
-      if (isLoading || isFetching) {
-        return <p>Loading...</p>; // Or show a spinner
-      }
-      if (!authentication && authUser) {
-    return <Navigate to="/" replace />;
-  }
-
-    return <>{children}</>;
+if (!authentication && authUser) {
+	return <Navigate to='/' replace />;
 }
 
-export default ProtectedRoute
+return <>{children}</>;
+
+};
+export default ProtectedRoute;
