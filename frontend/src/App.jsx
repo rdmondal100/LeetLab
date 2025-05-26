8910e36f8814432c90dbd0b40f69c156c0c9cd32
Riday <rdmondal100@gmail.com>
2025-05-26 08:46:09 +0600

    update: moved toster from main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,17 @@ import HomePage from './pages/HomePage'
 import RegisterPage from './pages/authPages/RegisterPage'
 import LoginPage from './pages/authPages/LoginPage'
 import ProtectedRoute from './components/ProtectedRoute'
+
+import { Toaster } from "@/components/ui/sonner"
+
 const App  = ()=>{
 
 
 
   return (
   <div className="flex flex-col items-center justify-start ">
-    
+        <Toaster richColors    position="top-center" />
+
     <Routes>
       {/* auth  */}
       <Route
