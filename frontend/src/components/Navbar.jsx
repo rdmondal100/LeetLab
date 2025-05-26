92a2c449b45dd44c95f8e898936b00c239d2b585
Riday <rdmondal100@gmail.com>
2025-05-26 14:16:56 +0600

    update: dropdown menu update with add problem and icons

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Profiler } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,18 +9,17 @@ import {
 	DropdownMenuLabel,
 	DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
-import { UserCircle, Search, } from "lucide-react";
+import { UserCircle, Search, Code, LogOut } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avater";
 import { useSelector } from "react-redux";
 import { avaterPlaceholder } from "../assets";
 
 const Navbar = () => {
-    const authUser = useSelector((state)=>state.auth?.authUser?.data)
-    console.log(authUser)
+	const authUser = useSelector((state) => state.auth?.authUser?.data);
+	console.log(authUser);
 	return (
 		<nav className='w-full container bg-background px-6 py-3 flex items-center justify-between '>
-			{/* Left: Logo */}
 			<Link
 				to='/'
 				className='text-2xl font-extrabold tracking-tight text-primary flex items-center gap-1'
@@ -31,7 +30,6 @@ const Navbar = () => {
 				<span className='text-muted-foreground'>Battle</span>
 			</Link>
 
-			{/* Center: Menu */}
 			<div className='hidden lg:flex gap-4 text-sm font-medium text-muted-foreground'>
 				<Button variant='ghost' className='hover:text-primary'>
 					Problems
@@ -47,7 +45,6 @@ const Navbar = () => {
 				</Button>
 			</div>
 
-			{/* Right: Search + Profile */}
 			<div className='flex items-center gap-2'>
 				<div className='relative'>
 					<Input
@@ -60,17 +57,29 @@ const Navbar = () => {
 
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild>
-                    <Avatar className="cursor-pointer">
-      <AvatarImage src={authUser?.image} alt={authUser?.name} />
-      <AvatarFallback> <img src={avaterPlaceholder} alt={authUser?.name} /> </AvatarFallback>
-    </Avatar>
+						<Avatar className='cursor-pointer'>
+							<AvatarImage src={authUser?.image} alt={authUser?.name} />
+							<AvatarFallback>
+								{" "}
+								<img src={avaterPlaceholder} alt={authUser?.name} />{" "}
+							</AvatarFallback>
+						</Avatar>
 					</DropdownMenuTrigger>
-					<DropdownMenuContent align='end' className='w-40'>
+					<DropdownMenuContent align='end' className='w-40 flex flex-col gap-1.5'>
 						<DropdownMenuLabel>My Account</DropdownMenuLabel>
 						<DropdownMenuSeparator />
-						<DropdownMenuItem>Profile</DropdownMenuItem>
-						<DropdownMenuItem>Settings</DropdownMenuItem>
-						<DropdownMenuItem>Logout</DropdownMenuItem>
+						<DropdownMenuItem className='flex cursor-pointer items-center'>
+							<UserCircle />
+							Profile
+						</DropdownMenuItem>
+						<DropdownMenuItem className='flex cursor-pointer items-center '>
+							<Code />
+							Add Problem
+						</DropdownMenuItem>
+						<DropdownMenuItem className='flex cursor-pointer  items-center '>
+							<LogOut />
+							Logout
+						</DropdownMenuItem>
 					</DropdownMenuContent>
 				</DropdownMenu>
 			</div>
