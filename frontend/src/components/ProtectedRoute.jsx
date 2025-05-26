273f99c0405c5516c2f65363fd67095ed0653dc8
Riday <rdmondal100@gmail.com>
2025-05-26 08:43:54 +0600

    add: implement the protectedRoutes functionality

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useGetAuthUserQuery } from '../redux-toolkit/services/authService';
+import { useDispatch } from 'react-redux';
+import { setAuthUser } from '../redux-toolkit/features/authSlice';
+import { Navigate } from 'react-router-dom';
 
 
-const ProtectedRoute = ({ children, authentication}) => {
+const ProtectedRoute = ({ children, authentication = true}) => {
     console.log(authentication)
+    const {
+        data: authUser,
+        isFetching,
+        isLoading,
+        error
+      } = useGetAuthUserQuery()
+
+      const dispatch = useDispatch()
+    
+      useEffect(() => {
+        if (authUser) {
+          dispatch(setAuthUser(authUser));
+        }
+      }, [authUser, dispatch]);
+
+
+      if (isLoading || isFetching) {
+        return <p>Loading...</p>; // Or show a spinner
+      }
+      if (!authentication && authUser) {
+    return <Navigate to="/" replace />;
+  }
+
     return <>{children}</>;
 }
 
