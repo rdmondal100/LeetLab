708de41e33a3d24f13420fa6915c151fe30dbc19
Riday <rdmondal100@gmail.com>
2025-05-26 20:51:34 +0600

    fix: redirections logic rewrite

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,40 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useGetAuthUserQuery } from "../redux-toolkit/services/authService";
 import { useDispatch } from "react-redux";
 import { setAuthUser } from "../redux-toolkit/features/authSlice";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children, authentication = true }) => {
-	console.log(authentication);
+	const [authChecked, setAuthChecked] = useState(false);
+
 	const {
 		data: authUser,
 		isFetching,
 		isLoading,
 		error,
 	} = useGetAuthUserQuery();
-
+	console.log(authUser);
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		if (authUser) {
-			dispatch(setAuthUser(authUser));
+		if (authUser?.success) {
+			dispatch(setAuthUser(authUser?.data));
+		} else {
+			dispatch(setAuthUser(null));
 		}
-	}, [authUser, dispatch]);
 
-	if (isLoading || isFetching) {
-		return <p>Loading...</p>; // Or show a spinner
+		if (!isLoading && !isFetching) {
+			setAuthChecked(true);
+		}
+	}, [authUser, isLoading, isFetching]);
+
+	if (!authChecked) {
+		return <p>Loading...</p>;
 	}
 
-	if (authentication && !authUser && !isLoading && !isFetching) {
+	if (authentication && !authUser?.success) {
 		return <Navigate to='/login' replace />;
 	}
 
+	if (!authentication && authUser?.success) {
+		return <Navigate to='/' replace />;
+	}
 
-if (!authentication && authUser) {
-	return <Navigate to='/' replace />;
-}
-
-return <>{children}</>;
-
+	return <>{children}</>;
 };
 export default ProtectedRoute;
