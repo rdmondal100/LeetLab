6c84e80702f894f40dbc38b1fe423fba1cb97b84
Riday <rdmondal100@gmail.com>
2025-05-29 19:41:54 +0600

    fix: return the userId with AllProblems

--- backend/src/controllers/problem.controller.js
+++ backend/src/controllers/problem.controller.js
@@ -94,7 +94,15 @@ export const createProblem = asyncHandler(async (req, res) => {
 
 export const getAllProblems = asyncHandler(async (req, res) => {
 
-    const problems = await db.problem.findMany()
+    const problems = await db.problem.findMany({
+        include:{
+            solvedBy:{
+                where:{
+                    userId:req?.user?.id
+                }
+            }
+        }
+    })
     console.log("Got all problems", problems)
     if (!problems) {
         throw new ApiError(404, "No problem found")
