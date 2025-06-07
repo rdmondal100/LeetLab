f9e206326cac46ec52b48df6b5c0d1c77218d9aa
Riday Mondal <rdmondal100@gmail.com>
2025-06-07 17:17:53 +0600

    deployment

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,3 +1,3 @@
 
-export const    BASEURL  =  import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1": "/api/v1"
+export const    BASEURL  =  import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1": "https://dsabattle-server.vercel.app/api/v1"
 
