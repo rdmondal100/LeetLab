aac2b8a5e5219098437b4a00300ebbd304d15de0
Riday Mondal <rdmondal100@gmail.com>
2025-06-07 17:28:27 +0600

    update domian

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "https://dsabattle.vercel.app",
+    origin: "https://www.dsabattle.com",
     credentials: true
 }))
 
