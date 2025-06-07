ba64c8d71c9ed6003a38fd0d8113aa9553a4b9eb
Riday Mondal <rdmondal100@gmail.com>
2025-06-07 23:26:27 +0600

    update: backend url

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -2,5 +2,5 @@
 // export const    BASEURL  =  import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1": "https://dsabattle-server.vercel.app/api/v1"
 
 
-export const    BASEURL= "https://dsabattle-server.vercel.app/api/v1"
+export const    BASEURL= "https://dsabatte-server.onrender.com/api/v1"
 
