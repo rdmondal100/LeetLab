59b4b9db47fe0dd1205ebf8489cca5aad8f29813
Riday Mondal <rdmondal100@gmail.com>
2025-06-07 21:58:33 +0600

    bug:  import comment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,13 @@ import express from 'express'
 import dotenv from "dotenv"
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-
-import authRoutes from './routes/auth.route.js'
 import globalErrorHandler from './middleware/globalErrorHandler.js'
-import problemRoutes from './routes/problem.route.js'
-import executionRoutes from './routes/executeCode.route.js'
-import submissionRoutes from './routes/submission.route.js'
-import playlistRoutes from './routes/playlist.route.js'
+
+// import authRoutes from './routes/auth.route.js'
+// import problemRoutes from './routes/problem.route.js'
+// import executionRoutes from './routes/executeCode.route.js'
+// import submissionRoutes from './routes/submission.route.js'
+// import playlistRoutes from './routes/playlist.route.js'
 
 dotenv.config()
 
